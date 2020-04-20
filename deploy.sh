0bf7854233a2ee62697e5e975786b2582e7999e0
Trey Pendragon <tpendragon@princeton.edu>
2020-04-20 13:12:01 -0700

    Update deploy script.

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,16 @@ if [ $1 == "production" ]
 then
   sam deploy --stack-name=image-metadata-extractor-production \
   --s3-prefix=image-metadata-extractor-production \
-  --parameter-overrides='StageName="production", SourceBucket="iiif-image-production", Alias="iiif-cloud.princeton.edu"' \
+  --parameter-overrides='StageName="production", SourceBucket="iiif-image-production""' \
   --s3-bucket=aws-sam-cli-managed-default-samclisourcebucket-1j1ve93v4jqs9 \
   --region='us-east-1' \
   --capabilities='CAPABILITY_IAM' \
   --profile='figgy-deploy'
 elif [ $1 == "staging" ]
 then
-  sam deploy --stack-name=iiif-serverless-staging \
-  --s3-prefix=iiif-serverless-staging \
-  --parameter-overrides='StageName="staging", SourceBucket="iiif-image-staging", Alias="iiif-cloud-staging.princeton.edu"' \
+  sam deploy --stack-name=image-metadata-extractor-staging \
+  --s3-prefix=image-metadata-extractor-staging \
+  --parameter-overrides='StageName="staging", SourceBucket="iiif-image-staging"' \
   --s3-bucket=aws-sam-cli-managed-default-samclisourcebucket-1j1ve93v4jqs9 \
   --region='us-east-1' \
   --capabilities='CAPABILITY_IAM' \
