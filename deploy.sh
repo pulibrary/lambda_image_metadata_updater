8f9d1968940ecf7da812962a65e7a8e71a5ef098
Trey Pendragon <tpendragon@princeton.edu>
2020-04-20 12:40:24 -0700

    WIP.

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+if [ $1 == "production" ]
+then
+  sam deploy --stack-name=image-metadata-extractor-production \
+  --s3-prefix=image-metadata-extractor-production \
+  --parameter-overrides='StageName="production", SourceBucket="iiif-image-production", Alias="iiif-cloud.princeton.edu"' \
+  --s3-bucket=aws-sam-cli-managed-default-samclisourcebucket-1j1ve93v4jqs9 \
+  --region='us-east-1' \
+  --capabilities='CAPABILITY_IAM' \
+  --profile='figgy-deploy'
+elif [ $1 == "staging" ]
+then
+  sam deploy --stack-name=iiif-serverless-staging \
+  --s3-prefix=iiif-serverless-staging \
+  --parameter-overrides='StageName="staging", SourceBucket="iiif-image-staging", Alias="iiif-cloud-staging.princeton.edu"' \
+  --s3-bucket=aws-sam-cli-managed-default-samclisourcebucket-1j1ve93v4jqs9 \
+  --region='us-east-1' \
+  --capabilities='CAPABILITY_IAM' \
+  --profile='figgy-deploy'
+else
+  echo 'Please enter either production or staging as the environment'
+fi
